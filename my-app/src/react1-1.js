3261aaed9b89d847b75bd1129f4dc8572471d68f
福原優太 <fuyu0853@icloud.com>
2023-11-06 13:15:59 +0900

    file_create

---
+++ my-app/src/react1-1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewComponent() {
+    return ( <
+        div >
+        <
+        h1 > 新しいコンポーネントです！ < /h1>  <
+        /div>
+    );
+}
+
+export default NewComponent;
