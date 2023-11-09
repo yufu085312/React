aeade99ec5b06af016a85141fd159f1ef86a9509
福原優太 <fuyu0853@icloud.com>
2023-11-09 09:53:03 +0900

    fille change

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Profile() {
     return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
+        img src = "https://i.imgur.com/MK3eW3As.jpg"
+        alt = "Katherine Johnson" /
+        >
     );
 }
 
-export default App;
+export default function Gallery() {
+    return ( <
+        section >
+        <
+        h1 > Amazing scientists < /h1> <
+        Profile / >
+        <
+        Profile / >
+        <
+        Profile / >
+        <
+        /section>
+    );
+}
